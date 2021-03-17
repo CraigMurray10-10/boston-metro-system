b9f7f8c7651ffd1caf504c619683d694852603db
Claire Higgins <claireeh2000@googlemail.com>
2021-03-17 19:25:24 +0000

    breadth first search began implementation - just now only returns boolean if path exists.

--- src/BostonMetroSystem/MVCRouteFinder.java
+++ src/BostonMetroSystem/MVCRouteFinder.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class MVCRouteFinder {
     public static void main(String[] args){
+        /*
 
         RouteFinderView theView = new RouteFinderView();
 
@@ -17,6 +18,88 @@ public class MVCRouteFinder {
 
         parse.readFile();
         parse.parseFile();
+
+         */
+
+        MetroGraph graph = new MetroGraph();
+
+        Station s1 = new Station(1, "Station1");
+        Station s2 = new Station(2, "Station2");
+        Station s3 = new Station(3, "Station3");
+        Station s4 = new Station(4, "Station4");
+        Station s5 = new Station(5, "Station5");
+        Station s6 = new Station(6, "Station6");
+
+        graph.addVertex(s1);
+        graph.addVertex(s2);
+        graph.addVertex(s3);
+        graph.addVertex(s4);
+        graph.addVertex(s5);
+        graph.addVertex(s6);
+
+        graph.addEdge(s1,s2, "red") ;
+        graph.addEdge(s1,s3, "blue");
+        graph.addEdge(s1,s5, "orange");
+        graph.addEdge(s2,s4, "red");
+        graph.addEdge(s2,s5, "orange");
+        graph.addEdge(s3,s5, "blue");
+
+
+
+
+
+        RouteCalculator rc = new RouteCalculator(graph);
+
+        Boolean found = rc.calculateRoute(s1, s5);
+        System.out.println(found);
+         /*
+        List<StationColorPair> s1AdjVertices = graph.getAdjVertices(s1);
+        List<StationColorPair> s2AdjVertices = graph.getAdjVertices(s2);
+        List<StationColorPair> s3AdjVertices = graph.getAdjVertices(s3);
+        List<StationColorPair> s4AdjVertices = graph.getAdjVertices(s4);
+        List<StationColorPair> s5AdjVertices = graph.getAdjVertices(s5);
+
+        System.out.println("---Station 1 Adj Vertices ----");
+        for(int i = 0; i < s1AdjVertices.size(); i++){
+            System.out.println(s1AdjVertices.get(i).getKey().getID());
+            System.out.println(s1AdjVertices.get(i).getKey().getStation());
+            System.out.println(s1AdjVertices.get(i).getValue());
+        }
+
+        System.out.println();
+        System.out.println("---Station 2 Adj Vertices ----");
+        for(int i = 0; i < s2AdjVertices.size(); i++){
+            System.out.println(s2AdjVertices.get(i).getKey().getID());
+            System.out.println(s2AdjVertices.get(i).getKey().getStation());
+            System.out.println(s2AdjVertices.get(i).getValue());
+        }
+
+        System.out.println();
+        System.out.println("---Station 3 Adj Vertices ----");
+        for(int i = 0; i < s3AdjVertices.size(); i++){
+            System.out.println(s3AdjVertices.get(i).getKey().getID());
+            System.out.println(s3AdjVertices.get(i).getKey().getStation());
+            System.out.println(s3AdjVertices.get(i).getValue());
+        }
+
+        System.out.println();
+        System.out.println("---Station 4 Adj Vertices ----");
+        for(int i = 0; i < s4AdjVertices.size(); i++){
+            System.out.println(s4AdjVertices.get(i).getKey().getID());
+            System.out.println(s4AdjVertices.get(i).getKey().getStation());
+            System.out.println(s4AdjVertices.get(i).getValue());
+        }
+
+        System.out.println();
+        System.out.println("---Station 5 Adj Vertices ----");
+        for(int i = 0; i < s5AdjVertices.size(); i++){
+            System.out.println(s5AdjVertices.get(i).getKey().getID());
+            System.out.println(s5AdjVertices.get(i).getKey().getStation());
+            System.out.println(s5AdjVertices.get(i).getValue());
+        }
+
+         */
+
 
 
 
--- src/BostonMetroSystem/RouteCalculator.java
+++ src/BostonMetroSystem/RouteCalculator.java
@@ -3,10 +3,36 @@ package BostonMetroSystem;
 import java.util.*;
 
 public class RouteCalculator {
-    public RouteCalculator(Station stationTo, Station stationFrom){
+
+    private MetroGraph metroSystem;
+    public RouteCalculator(MetroGraph graph){
+        this.metroSystem = graph;
+
     }
 
-    public List<Station> calculateRoute(Station station1, Station station2){
-        return null;
+    public Boolean calculateRoute(Station source, Station destination){
+        int count = 0;
+        List<Station> agenda = new ArrayList<>();
+        agenda.add(source);
+
+        //count is hard coded limit - used number of stations as would assume this would be longest
+        //route length possible
+        while (!agenda.isEmpty() && count < 125) {
+            Station current = agenda.get(0);
+            count +=1;
+            agenda.remove(current);
+
+            if (destination == current) {
+                return true;
+            }else{
+                List<StationColorPair> nextStatesPairs = metroSystem.getAdjVertices(current);
+
+                //adds all stations to agenda
+                for(int i = 0; i < nextStatesPairs.size(); i++){
+                    agenda.add(nextStatesPairs.get(i).getKey());
+                }
+            }
+        }
+        return false;
     }
 }
